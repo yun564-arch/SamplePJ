7934d0b728855bc75924637133d3bc2ff7599f55
yun564-arch <yunat564@gmail.com>
2026-06-26 17:29:49 +0900

    refactor: HomePageControllerをコンストラクタインジェクションに統一

--- samplePJ/src/main/java/com/example/demo/controller/HomePageController.java
+++ samplePJ/src/main/java/com/example/demo/controller/HomePageController.java
@@ -1,22 +1,22 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomePageController {
-	
-	@Autowired
-	HttpSession session;
 
+    private final HttpSession session;
+
+    public HomePageController(HttpSession session) {
+        this.session = session;
+    }
 
     // ホームページを表示するメソッド
     @RequestMapping("/")
     public String index() {
-    	session.invalidate();
-    	return "homePage";
-
+        session.invalidate();
+        return "homePage";
     }
 }
