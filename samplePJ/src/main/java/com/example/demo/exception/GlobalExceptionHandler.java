f9d091cfadea236bdb521c8d5c966163b5d78c79
yun564-arch <yunat564@gmail.com>
2026-07-03 18:45:07 +0900

    fix: GlobalExceptionHandlerのprintStackTraceをSLF4Jロガーに置き換え

--- samplePJ/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ samplePJ/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.demo.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +9,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     // タスクが見つからない場合(存在しない or 他人のもの)
     @ExceptionHandler(TaskNotFoundException.class)
@@ -20,8 +24,7 @@ public class GlobalExceptionHandler {
     // 想定外の例外(バグなど)
     @ExceptionHandler(Exception.class)
     public ModelAndView handleOther(Exception e) {
-        // ログには詳細を出す(画面には出さない)
-        e.printStackTrace();
+        logger.error("予期しない例外が発生しました", e);
         ModelAndView mv = new ModelAndView("error/error");
         mv.addObject("message", "予期しないエラーが発生しました");
         mv.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
